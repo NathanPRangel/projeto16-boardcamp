202ab7cab766a9f306da8eead43fe9ab993ea353
Nathan Rangel <nathanpk20@gmail.com>
2023-12-11 10:35:00 -0300

    indent adjustment

--- src/controllers/Customers.js
+++ src/controllers/Customers.js
@@ -1,124 +1,134 @@
-import connection from "../db.js"
-import catchError from "../error/catchError.js"
-import { offsetLimit, setLimit, setOffset } from "../utils/offsetLimit.js"
-import { setOrder, sortItems } from "../utils/order.js"
-import { utilCustomer } from "../utils/utilMap.js"
+import connection from "../db.js";
+import catchError from "../error/catchError.js";
+import { offsetLimit, setLimit, setOffset } from "../utils/offsetLimit.js";
+import { setOrder, sortItems } from "../utils/order.js";
+import { utilCustomer } from "../utils/utilMap.js";
 
 export async function allCustomers(req, res) {
+  const { cpf, limit, offset, order, desc } = req.query;
 
-    const { cpf, limit, offset, order, desc } = req.query
+  offsetLimit(offset, limit);
 
-    offsetLimit(offset, limit)
+  const sortByFilters = {
+    id: 1,
+    name: 2,
+    phone: 3,
+    cpf: 4,
+    birthday: 5,
+    rentalsCount: 6,
+  };
 
-    const sortByFilters = {
-        id: 1,
-        name: 2,
-        phone: 3,
-        cpf: 4,
-        birthday: 5,
-        rentalsCount: 6
+  sortItems(order, desc, sortByFilters);
+
+  try {
+    if (cpf) {
+      const arrCustomers = await connection.query(
+        `
+        SELECT * FROM customers WHERE cpf LIKE $1
+      `,
+        [`${cpf}%`]
+      );
+
+      const mappedCustomers = utilCustomer(arrCustomers);
+      res.send(mappedCustomers);
+    } else {
+      const arrCustomers = await connection.query(
+        `
+        SELECT * FROM customers
+          ${setOffset}
+          ${setLimit}
+          ${setOrder}
+      `
+      );
+
+      const mappedCustomers = utilCustomer(arrCustomers);
+      res.send(mappedCustomers);
     }
-
-    sortItems(order, desc, sortByFilters)
-
-    try {
-
-        if (cpf) {
-            let arrCustomers = await connection.query(`
-                SELECT * FROM customers WHERE cpf LIKE $1
-            `, [`${cpf}%`])
-
-            arrCustomers = utilCustomer(arrCustomers)
-
-            res.send(arrCustomers)
-        }
-
-        let arrCustomers = await connection.query(`
-            SELECT * FROM customers
-                ${setOffset}
-                ${setLimit}
-                ${setOrder}
-        `)
-
-        arrCustomers = utilCustomer(arrCustomers)
-
-        res.send(arrCustomers)
-
-    } catch (error) {
-        catchError(res, error)
-    }
+  } catch (error) {
+    catchError(res, error);
+  }
 }
 
 export async function newCustomer(req, res) {
-    const { name, phone, cpf, birthday } = req.body
+  const { name, phone, cpf, birthday } = req.body;
 
-    try {
+  try {
+    const customer = await connection.query(
+      `
+      SELECT * FROM customers WHERE cpf = ($1)
+    `,
+      [cpf]
+    );
 
-        const { rows: customer } = await connection.query(`
-            SELECT * FROM customers WHERE cpf = ( $1 )
-        `, [cpf])
+    if (customer.rows.length > 0) return res.sendStatus(409);
 
-        if (customer.length > 0) return res.sendStatus(409)
+    await connection.query(
+      `
+      INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)
+    `,
+      [name, phone, cpf, birthday]
+    );
 
-        await connection.query(`
-            INSERT INTO customers ( name, phone, cpf, birthday ) VALUES ( $1, $2, $3, $4 )
-        `, [name, phone, cpf, birthday])
-
-        res.sendStatus(201)
-
-    } catch (error) {
-        catchError(res, error)
-    }
-
+    res.sendStatus(201);
+  } catch (error) {
+    catchError(res, error);
+  }
 }
 
 export async function selectedCustomer(req, res) {
-    const { id } = req.params
+  const { id } = req.params;
 
-    try {
+  try {
+    const customerById = await connection.query(
+      `
+      SELECT * FROM customers WHERE id = ($1)
+    `,
+      [id]
+    );
 
-        let customerById = await connection.query(`
-            SELECT * FROM customers WHERE id = ( $1 )
-        `, [id])
+    if (customerById.rows.length === 0) return res.sendStatus(404);
 
-        if (customerById.rows.length === 0) return res.sendStatus(404)
-
-        customerById = utilCustomer(customerById)
-
-        res.send(customerById[0])
-
-    } catch (error) {
-        catchError(res, error)
-    }
+    const mappedCustomer = utilCustomer(customerById);
+    res.send(mappedCustomer[0]);
+  } catch (error) {
+    catchError(res, error);
+  }
 }
 
 export async function updateCustomer(req, res) {
-    const { id } = req.params
-    const { name, phone, cpf, birthday } = req.body
+  const { id } = req.params;
+  const { name, phone, cpf, birthday } = req.body;
 
-    try {
+  try {
+    const customer = await connection.query(
+      `
+      SELECT * FROM customers WHERE id=$1
+    `,
+      [id]
+    );
 
-        const { rows: customer } = await connection.query(`
-            SELECT * FROM customers WHERE id=$1
-        `, [id])
+    const cpfCustomer = await connection.query(
+      `
+      SELECT id FROM customers WHERE cpf=$1
+    `,
+      [cpf]
+    );
 
-        const { rows: cpfCustomer } = await connection.query(`
-            SELECT id FROM customers WHERE cpf=$1
-        `, [cpf])
-
-        if (cpfCustomer.length > 0 && cpf !== customer[0].cpf) {
-            return res.sendStatus(409)
-        }
-
-        await connection.query(`
-            UPDATE customers
-            SET name = $1, cpf = $2, phone = $3, birthday = $4 
-            WHERE id=$5
-        `, [name, cpf, phone, birthday, id])
-
-        res.sendStatus(200)
-
-    } catch (error) {
-        catchError(res, error)
+    if (cpfCustomer.rows.length > 0 && cpf !== customer[0].cpf) {
+      return res.sendStatus(409);
     }
+
+    await connection.query(
+      `
+      UPDATE customers
+      SET name = $1, cpf = $2, phone = $3, birthday = $4
+      WHERE id=$5
+    `,
+      [name, cpf, phone, birthday, id]
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    catchError(res, error);
+  }
 }
--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,55 +1,58 @@
-import connection from '../db.js'
-import catchError from '../error/catchError.js'
-import { offsetLimit, setLimit, setOffset } from '../utils/offsetLimit.js'
-import { setOrder, sortItems } from '../utils/order.js'
+import connection from '../db.js';
+import catchError from '../error/catchError.js';
+import { offsetLimit, setLimit, setOffset } from '../utils/offsetLimit.js';
+import { setOrder, sortItems } from '../utils/order.js';
 
 export async function allCategories(req, res) {
+  const { limit, offset, order, desc } = req.query;
 
-    const { limit, offset, order, desc } = req.query
+  offsetLimit(offset, limit);
 
-    offsetLimit(offset, limit)
+  const sortByFilters = {
+    id: 1,
+    name: 2,
+  };
 
-    const sortByFilters = {
-        id: 1,
-        name: 2
-    }
+  sortItems(order, desc, sortByFilters);
 
-    sortItems(order, desc, sortByFilters)
+  try {
+    const { rows: arrCategories } = await connection.query(
+      `
+      SELECT * FROM categories
+        ${setOffset}
+        ${setLimit}
+        ${setOrder}
+    `,
+    );
 
-    try {
-
-        const { rows: arrCategories } = await connection.query(`
-            SELECT * FROM categories
-                ${setOffset}
-                ${setLimit}
-                ${setOrder}
-        `)
-
-        res.send(arrCategories)
-
-    } catch (error) {
-        catchError(res.error)
-    }
+    res.send(arrCategories);
+  } catch (error) {
+    catchError(res.error);
+  }
 }
 
 export async function newCategory(req, res) {
-    const { name } = req.body
+  const { name } = req.body;
 
-    try {
+  try {
+    const category = await connection.query(
+      `
+      SELECT * FROM categories WHERE name = ($1)
+    `,
+      [name],
+    );
 
-        const category = await connection.query(`
-            SELECT * FROM categories WHERE name = ( $1 )
-        `, [name])
+    if (category.rows.length > 0) return res.sendStatus(409);
 
-        if (category.rows.length > 0) return res.sendStatus(409)
+    await connection.query(
+      `
+      INSERT INTO categories (name) VALUES ($1)
+    `,
+      [name],
+    );
 
-        await connection.query(`
-            INSERT INTO categories ( name ) VALUES ( $1 )
-        `, [name])
-
-        res.sendStatus(201)
-
-    } catch (error) {
-        catchError(res, error)
-    }
+    res.sendStatus(201);
+  } catch (error) {
+    catchError(res, error);
+  }
 }
